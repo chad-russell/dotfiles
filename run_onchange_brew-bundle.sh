8aff7cf08905588125383c138f898dc8c8cedbdd
Chad Russell <chaddouglasrussell@gmail.com>
2025-05-15 12:15:08 -0400

    Update chezmoi

--- run_onchange_brew-bundle.sh
+++ run_onchange_brew-bundle.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-# Run brew bundle install to install/update packages from Brewfile
-# The --file argument points to the Brewfile in the chezmoi source directory.
-echo "Executing: brew bundle install --file {{ .chezmoi.sourceDir }}/Brewfile"
-brew bundle install --file "{{ .chezmoi.sourceDir }}/Brewfile"
+echo "Attempting to run: brew bundle install --file $HOME/Brewfile --verbose"
+echo "Checking if $HOME/Brewfile exists and is a symlink:"
+ls -l "$HOME/Brewfile"
+
+# Run brew bundle install, pointing to the Brewfile symlinked by chezmoi in the home directory.
+brew bundle install --file "$HOME/Brewfile" --verbose
