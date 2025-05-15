24fe65708f351b65f0e4cdd6735b96e9524d8a72
Chad Russell <chaddouglasrussell@gmail.com>
2025-05-15 13:39:26 -0400

    Update chezmoi

--- run_once_before_install-homebrew.sh
+++ run_once_before_install-homebrew.sh
@@ -4,7 +4,7 @@
 if ! command -v brew >/dev/null 2>&1; then
   echo "Homebrew not found. Installing Homebrew..."
   # Install Homebrew
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   echo "*** SOURCING HOMEBREW!"
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 else
